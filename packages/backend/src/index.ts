301f7faf892af9628c1a9b2eb80e4c8545032178
gzzengxianzhi <gzzengxianzhi@corp.netease.com>
2023-02-05 18:01:11 +0800

    chore: docker learning

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,16 +1,17 @@
 import express from "express";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res)=>{
         res.json({
             code: 200,
             result: {
-                message: "嘻嘻"
+                message: "嘻2嘻"
             }
         })
 })
 
-app.listen("8000", ()=>{
-    console.log("app listenning on 8000");
+app.listen(port, ()=>{
+    console.log(`app listenning on ${port}`);
 })
